97ea7a89284503c142e5961608746882babe6063
Stefan Daschek <stefan@die-antwort.eu>
2018-06-30 23:30:35 +0200

    Include soundboard

--- Almaphone.ino
+++ Almaphone.ino
@@ -1,18 +1,44 @@
 #include <avr/sleep.h>
+#include <SoftwareSerial.h>
 
+#include "Adafruit_Soundboard.h"
 #include "RotaryPhoneDialDecoder.h"
 
-static const uint8_t ROTARY_PHONE_A_PIN = 3;
-static const uint8_t ROTARY_PHONE_B_PIN = 4;
+static const uint8_t ROTARY_PHONE_A_PIN = A0;
+static const uint8_t ROTARY_PHONE_B_PIN = A1;
+static const uint8_t SFX_TX = 5;
+static const uint8_t SFX_RX = 6;
 
 RotaryPhoneDialDecoder rotaryPhoneDialDecoder(ROTARY_PHONE_A_PIN, ROTARY_PHONE_B_PIN);
+SoftwareSerial sfxSerial = SoftwareSerial(SFX_TX, SFX_RX);
+Adafruit_Soundboard sfx = Adafruit_Soundboard(&sfxSerial, NULL, SFX_TX); // Last argument is reset pin, which we don't use
 
 void setup() {
-  Serial.begin(57600);
   rotaryPhoneDialDecoder.setup();
+  pinMode(13, OUTPUT);
+  sfxSerial.begin(9600 * (16000000L / F_CPU)); // Because we run with 8 MHz,
+  delay(250);
+}
+
+void flushInput() {
+  // Read all available serial input to flush pending data.
+  uint16_t timeoutloop = 0;
+  while (timeoutloop++ < 40) {
+    while(sfxSerial.available()) {
+      sfxSerial.read();
+      timeoutloop = 0;  // If char was received reset the timer
+    }
+    delay(1);
+  }
 }
 
 void loop() {
+  char filename[] = "F       OGG";
+  flushInput();
+  sfx.playTrack(filename);
   uint8_t digit = rotaryPhoneDialDecoder.waitForDigit();
-  Serial.println(digit);
+  sfx.stop();
+  filename[0] = digit + '0';
+  sfx.playTrack(filename);
+  while(1);
 }
