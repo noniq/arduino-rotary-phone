c47adde35562879c34d6064cc4b29a9d2a4738f0
Stefan Daschek <stefan@die-antwort.eu>
2018-07-02 00:54:31 +0200

    Use hardware serial instead of software serial
    This is possible because we use the bare microcontroller (no Arduino board).

--- Almaphone.ino
+++ Almaphone.ino
@@ -1,22 +1,18 @@
 #include <avr/sleep.h>
-#include <SoftwareSerial.h>
 
 #include "Adafruit_Soundboard.h"
 #include "RotaryPhoneDialDecoder.h"
 
 static const uint8_t ROTARY_PHONE_A_PIN = A0;
 static const uint8_t ROTARY_PHONE_B_PIN = A1;
-static const uint8_t SFX_TX = 5;
-static const uint8_t SFX_RX = 6;
 
 RotaryPhoneDialDecoder rotaryPhoneDialDecoder(ROTARY_PHONE_A_PIN, ROTARY_PHONE_B_PIN);
-SoftwareSerial sfxSerial = SoftwareSerial(SFX_TX, SFX_RX);
-Adafruit_Soundboard sfx = Adafruit_Soundboard(&sfxSerial, NULL, SFX_TX); // Last argument is reset pin, which we don't use
+Adafruit_Soundboard sfx = Adafruit_Soundboard(&Serial, NULL, 9); // Last argument is reset pin, which we don't use
 
 void setup() {
   rotaryPhoneDialDecoder.setup();
   pinMode(13, OUTPUT);
-  sfxSerial.begin(9600 * (16000000L / F_CPU)); // Because we run with 8 MHz,
+  Serial.begin(9600);
   delay(250);
 }
 
@@ -24,8 +20,8 @@ void flushInput() {
   // Read all available serial input to flush pending data.
   uint16_t timeoutloop = 0;
   while (timeoutloop++ < 40) {
-    while(sfxSerial.available()) {
-      sfxSerial.read();
+    while(Serial.available()) {
+      Serial.read();
       timeoutloop = 0;  // If char was received reset the timer
     }
     delay(1);
