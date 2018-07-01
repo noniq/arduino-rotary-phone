18330a3816f3803cefac87883f1af7fb293321c4
Stefan Daschek <stefan@die-antwort.eu>
2018-07-02 00:56:07 +0200

    Go into sleep mode after sound has finished playing.

--- Almaphone.ino
+++ Almaphone.ino
@@ -5,12 +5,14 @@
 
 static const uint8_t ROTARY_PHONE_A_PIN = A0;
 static const uint8_t ROTARY_PHONE_B_PIN = A1;
+static const uint8_t SFX_ACT = 7;
 
 RotaryPhoneDialDecoder rotaryPhoneDialDecoder(ROTARY_PHONE_A_PIN, ROTARY_PHONE_B_PIN);
 Adafruit_Soundboard sfx = Adafruit_Soundboard(&Serial, NULL, 9); // Last argument is reset pin, which we don't use
 
 void setup() {
   rotaryPhoneDialDecoder.setup();
+  pinMode(SFX_ACT, INPUT);
   pinMode(13, OUTPUT);
   Serial.begin(9600);
   delay(250);
@@ -36,5 +38,16 @@ void loop() {
   sfx.stop();
   filename[0] = digit + '0';
   sfx.playTrack(filename);
-  while(1);
+  delay(500);
+  while(digitalRead(SFX_ACT) == LOW) {
+    delay(10);
+  }
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+  sleep_mode();
+  while(1) { // never reached (if sleep mode works correctly)
+    digitalWrite(13, LOW);
+    delay(200);
+    digitalWrite(13, HIGH);
+    delay(200);
+  }
 }
