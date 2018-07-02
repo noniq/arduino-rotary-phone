36c5ff2bcbca0c5e06ae90e1ac5949b2aecb0c13
Stefan Daschek <stefan@die-antwort.eu>
2018-07-02 18:39:58 +0200

    Support a LED for debugging

--- Almaphone.ino
+++ Almaphone.ino
@@ -3,6 +3,9 @@
 #include "Adafruit_Soundboard.h"
 #include "RotaryPhoneDialDecoder.h"
 
+#define DEBUG
+
+static const uint8_t DEBUG_LED_PIN = 13;
 static const uint8_t ROTARY_PHONE_A_PIN = A0;
 static const uint8_t ROTARY_PHONE_B_PIN = A1;
 static const uint8_t SFX_ACT = 7;
@@ -10,12 +13,25 @@ static const uint8_t SFX_ACT = 7;
 RotaryPhoneDialDecoder rotaryPhoneDialDecoder(ROTARY_PHONE_A_PIN, ROTARY_PHONE_B_PIN);
 Adafruit_Soundboard sfx = Adafruit_Soundboard(&Serial, NULL, 9); // Last argument is reset pin, which we don't use
 
+void debugBlink(uint16_t millis) {
+#ifdef DEBUG
+  digitalWrite(DEBUG_LED_PIN, LOW);
+  delay(millis);
+  digitalWrite(DEBUG_LED_PIN, HIGH);
+  delay(millis);
+#endif
+}
+
 void setup() {
   rotaryPhoneDialDecoder.setup();
   pinMode(SFX_ACT, INPUT);
-  pinMode(13, OUTPUT);
+#ifdef DEBUG
+  pinMode(DEBUG_LED_PIN, OUTPUT);
+  digitalWrite(DEBUG_LED_PIN, HIGH);
+#endif
   Serial.begin(9600);
   delay(250);
+  debugBlink(50);
 }
 
 void flushInput() {
@@ -42,12 +58,10 @@ void loop() {
   while(digitalRead(SFX_ACT) == LOW) {
     delay(10);
   }
+  pinMode(DEBUG_LED_PIN, INPUT);
   set_sleep_mode(SLEEP_MODE_PWR_DOWN);
   sleep_mode();
   while(1) { // never reached (if sleep mode works correctly)
-    digitalWrite(13, LOW);
-    delay(200);
-    digitalWrite(13, HIGH);
-    delay(200);
+    debugBlink(200);
   }
 }
